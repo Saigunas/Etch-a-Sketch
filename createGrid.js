595efbe8eaa583fc63cbf53623a255b829525e52
Saga <92161964+Saigunas@users.noreply.github.com>
2022-03-06 22:03:19 +0200

    Added slider that deletes grid items Also created another wrapper for grid items which hold default background color. Sliders halve the opacity of painted blocks.

--- createGrid.js
+++ createGrid.js
@@ -1,20 +1,57 @@
 const gridContainer = document.querySelector(".grid-container");
-const gridBackground = document.querySelector(".grid-background");
+const gridBackgroundBorder = document.querySelector(".grid-background");
 
-gridBackground.style.backgroundColor = gridBackgroundColor;
+gridBackgroundBorder.style.backgroundColor = gridBordersColor;
 gridContainer.style.gridTemplateColumns = `repeat(${gridItems}, auto)`;
 gridContainer.style.gridGap = `${gridGapWidth}px`;
 
-        //Create grid items
-for(let i = 0; i < gridItems * gridItems; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
+//Create grid items
+for (let i = 0; i < gridItems * gridItems; i++) {
+  //This will have default background color behind each grid item
+  //Needed for after grid items color erasure
+  const gridItemBackground = document.createElement("div");
+  gridItemBackground.style.backgroundColor = gridItemBackgroundColor;
 
-    gridItem.style.padding = gridItemWidth;
-    
-    gridContainer.appendChild(gridItem);
+  //Blocks start with the dafault color
+  const gridItem = document.createElement("div");
+  gridItem.style.backgroundColor = gridItemBackgroundColor;
+
+  gridItem.classList.add("grid-item");
+  gridItem.style.padding = gridItemWidth;
+
+  //setting coordinates so slider can erase specific blocks
+  let coordinates = `${i % gridItems}, ${Math.floor(i / gridItems)}`;
+  gridItem.setAttribute("data-coordinates", coordinates);
+
+  gridContainer.appendChild(gridItemBackground);
+  gridItemBackground.appendChild(gridItem);
 }
 
-//Doesn't allow client to drag the image, 
-//thus not messing up drawing.
-window.ondragstart = function() { return false; }
+//slider
+const gridSlider = document.querySelector(".grid-slider");
+
+gridSlider.style.width = `${gridWidth}px`;
+gridSlider.max = gridItems;
+
+//When input detected on slider, find the blocks based on sliders value
+//And halve the opacity for the magnetic board affect
+gridSlider.oninput = function () {
+  for (let i = 0; i < gridItems; i++) {
+    let blockToErase = document.querySelector(
+      `.grid-item[data-coordinates="${this.value}, ${i}"]`
+    );
+
+    if (blockToErase.style.backgroundColor == gridItemBackgroundColor) {
+      continue;
+    }
+
+    let opacityHalf =
+      blockToErase.style.backgroundColor == `rgba(${pencilColorRgb}, 0.5)`;
+
+    if (opacityHalf) {
+      blockToErase.style.backgroundColor = gridItemBackgroundColor;
+    } else {
+      blockToErase.style.backgroundColor = `rgba(${pencilColorRgb}, 0.5)`;
+    }
+  }
+};
--- gridConfig.js
+++ gridConfig.js
@@ -1,21 +1,28 @@
 let gridItems = 20;
 let gridWidth = 300;
 let gridGapWidth = 1;
-let gridBackgroundColor = "rgb(186, 247, 226)";
-let pencilColor = 'grey';
-
+let gridBordersColor = "rgb(186, 247, 226)";
+let gridItemBackgroundColor = "rgb(242, 242, 242)";
+let pencilColor = "rgb(143, 143, 143)";
+let pencilColorRgb = pencilColor.slice(4, -1);
 let gridItemWidth = calculateBlockW();
 
 function calculateBlockW() {
-    /*  
+  /*  
         Calculate block width so wanted grid width is
         absolute, no less, no more.
     */
 
-    let gridWidthNoGap;     //Minus one as there is no gap after last block in line
-    gridWidthNoGap = gridWidth - (gridGapWidth) * (gridItems - 1);
-                            //Devided because block padding starts from center to sides,
-                            //thus creating double of the width we want
-    let blockWidth = (gridWidthNoGap / gridItems) / 2;
-    return `${blockWidth}px`
+  let gridWidthNoGap; //Minus one as there is no gap after last block in line
+  gridWidthNoGap = gridWidth - gridGapWidth * (gridItems - 1);
+  //Devided because block padding starts from center to sides,
+  //thus creating double of the width we want
+  let blockWidth = gridWidthNoGap / gridItems / 2;
+  return `${blockWidth}px`;
 }
+
+//Doesn't allow client to drag the image,
+//thus not messing up drawing.
+window.ondragstart = function () {
+  return false;
+};
